4400b1439d14a1d741b5e65e5eb455c0755f7b70
Tomas Perez <tom@0x101.com>
2014-03-02 18:55:47 +0100

    New scripts

---
+++ xsssniper-example.sh
@@ -0,0 +1,2 @@
+# http://localhost:8080/webgoat/attack?Screen=360&menu=900%3Cimg%20src=%22http://technewstoday.net/wp-content/uploads/2013/02/hacker-attack.jpg%22%3E%3C/img%3E
+./xsssniper/xsssniper.py -u "http://localhost:8080/webgoat/attack?Screen=360&menu=900" --cookie="JSESSIONID=6D3E8C520717D4F5479DB23806266BAD"
